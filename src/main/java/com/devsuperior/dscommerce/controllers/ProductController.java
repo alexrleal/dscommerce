9f0f627a37b8e687757d879c6bb2a0f61058fe9d
Alexandre Leal <alexrleal0@gmail.com>
2024-09-30 15:05:16 -0300

    Get products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -3,10 +3,13 @@ package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping(value = "/products")
@@ -18,5 +21,10 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
