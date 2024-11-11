1aaf588b7c09f85338e4ed2dccd11bbe4eb7f087
Alexandre Leal <alexrleal0@gmail.com>
2024-11-11 13:13:53 -0300

    UserDetails, GrantedAuthority, not tested

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,13 +1,15 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -80,10 +82,6 @@ public class User {
         this.birthDate = birthDate;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
@@ -106,9 +104,10 @@ public class User {
         return Objects.hashCode(id);
     }
 
-    public Set<Role> getRoles() {
-        return roles;
+    public String getPassword() {
+        return password;
     }
+
 
     public void addRole(Role role) {
         roles.add(role);
@@ -119,5 +118,35 @@ public class User {
             if (role.getAuthority().equals(roleName)) return true;
         }
         return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
